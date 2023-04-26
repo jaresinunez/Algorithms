082f41a9082e5a10f2a5a27ab87bd0abf9535bd9
Jaresi Nunez <58448481+jaresinunez@users.noreply.github.com>
2023-04-26 14:04:46 -0500

    user input validation and beginings of algo

--- src/main/java/Assignment5/DijkstrasShortest.java
+++ src/main/java/Assignment5/DijkstrasShortest.java
@@ -29,7 +29,6 @@ public class DijkstrasShortest {
                 cont = false;
             }
         } while (cont);
-        System.out.println("hello world");
     }
 
 
@@ -46,5 +45,65 @@ public class DijkstrasShortest {
         } else {
             return sourceNode.toLowerCase().charAt(0) >= startNode && sourceNode.toLowerCase().charAt(0) <= endNode;
         }
+    }
+
+    private static void dijkstraAlgorithm(int[][] digraph, String source){
+        Vertice[] vertices = new Vertice[digraph.length]; // current distance of a vertice from the source
+        for (int i = 0; i < vertices.length; i++) { vertices[i] = new Vertice(); }
+        vertices[getSourceIndex(source)].distSrc = 0;
+
+        int count = 0;
+        while(count < vertices.length){
+            Vertice v = vertices[getMinVertice(vertices)];
+
+            count++;
+        }
+
+    }
+
+    private static int getMinVertice(Vertice[] vertices){
+        int min = 0;
+        for (int i = 0; i < vertices.length-2; i++) {
+            if (vertices[i].distSrc < vertices[i+1].distSrc)
+                min = i;
+        }
+        return min;
+    }
+
+    /**
+     * return the source node's index in the digraph
+     * @param source source node
+     * @return source index
+     */
+    private static int getSourceIndex(String source) {
+        return switch (source.toLowerCase()) {
+            case "a" -> 0;
+            case "b" -> 1;
+            case "c" -> 2;
+            case "d" -> 3;
+            case "e" -> 4;
+            case "f" -> 5;
+            case "g" -> 6;
+            case "h" -> 7;
+            case "i" -> 8;
+            case "j" -> 9;
+            case "k" -> 10;
+            case "l" -> 11;
+            case "m" -> 12;
+            case "n" -> 13;
+            case "o" -> 14;
+            case "p" -> 15;
+            case "q" -> 16;
+            case "r" -> 17;
+            case "s" -> 18;
+            case "t" -> 19;
+            case "u" -> 20;
+            case "v" -> 21;
+            case "w" -> 22;
+            case "x" -> 23;
+            case "y" -> 24;
+            case "z" -> 25;
+            default -> -1;
+        };
     }
 }
---
+++ src/main/java/Assignment5/Vertice.java
@@ -0,0 +1,7 @@
+package Assignment5;
+
+public class Vertice {
+    int distSrc = Integer.MAX_VALUE;
+    boolean checked = false;
+    String from = "";
+}
